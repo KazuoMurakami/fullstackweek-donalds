1436f9578abd993bdad23bd9e7a9915500d92cdf
KazuoMurakami <otaviokazuo12@hotmail.com>
2025-02-19 21:02:25 -0300

    chore: update code style for consistency and add restaurant page component

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 
 const ProductPage = () => {
   return (
@@ -8,7 +8,7 @@ const ProductPage = () => {
       <Button>FSW 7.0</Button>
       <Input placeholder="Bora fechar esse projeto!" />
     </div>
-  );
-};
+  )
+}
 
-export default ProductPage;
+export default ProductPage
